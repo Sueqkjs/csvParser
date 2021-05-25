d7e398b671360af2677555852051631d854d66eb
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-26 07:58:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,19 @@
   };
 }());
 
-function Parser(input){
-  if(typeof input !== 'string') return [String(input)];
-  return input
-  .split("\n")
-  .map(function(x){ return x.split(",") })
-  .map(function(x){ return x.filter(function(y){ return y !== '' }) })
-  .map(function(x){ return x.map(function(y){ return y.replace('\r','') }) })
-  .filter(function(x){ return !!x && !!x.length })
+function Parser(){
+  var that = {};
+  
+  that["parse"] = function parse(input){
+    return input.split("\n")
+      .map(function(x){ return x.split(",") })
+      .map(function(x){ return x.filter(function(y){ return y !== '' }) })
+      .map(function(x){ return x.map(function(y){ return y.replace('\r','') }) })
+      .filter(function(x){ return !!x && !!x.length });
+  };
+  that["stringify"] = function stringify(input){
+    return input.map(x => x.join(','))
+    .join('\n');
+  });
+  return that;
 };
