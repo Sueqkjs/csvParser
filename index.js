8dbc0eec7681253151fd4ac9f3fdc93a41434dca
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-05-27 06:59:41 +0900

    ん〜

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 (function(){
   try {
     if(window.console) void 0;
@@ -8,20 +9,24 @@
 }());
 
 function Parser(){
-  var that = {};
+  var that = Object.create(null);
   
-  that["parse"] = function parse(input, reviver){
-    if(typeof reviver !== "function") reviver = new Function('return void 0;');
-    return input.split("\n")
-      .map(function(x){ return reviver(x, 0) || x.split(",") })
-      .map(function(x){ return reviver(x, 1) || x.filter(function(y){ return reviver(y, 2) || y !== '' }) })
-      .map(function(x){ return reviver(x, 3) || x.map(function(y){ return reviver(y, 4) || y.replace('\r','') }) })
-      .filter(function(x){ return !!x && !!x.length });
-  };
-  that["stringify"] = function stringify(input){
-    return input.map(x => x.join())
-    .join('\n');
-  };
+  Object.defineProperty(that, "parse", {
+    value: function parse(input, reviver){
+      if(typeof reviver !== "function") reviver = new Function('return void 0;');
+        return input.split("\n")
+          .map(function(x){ return reviver(x, 0) || x.split(",") })
+          .map(function(x){ return reviver(x, 1) || x.filter(function(y){ return reviver(y, 2) || y !== "" }) })
+          .map(function(x){ return reviver(x, 3) || x.map(function(y){ return reviver(y, 4) || y.replace('\r", "") }) })
+          .filter(function(x){ return !!x && !!x.length });
+    }
+  });
+  Object.defineProperty(that, "stringify", {
+    value: function stringify(input){
+      return input.map(x => x.join())
+        .join("\n");
+    };
+  });
   Object.freeze(that);
   Object.seal(that);
   return that;
