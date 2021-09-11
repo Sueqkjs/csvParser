933116fc406a0f7062e6228eb955c15f76d36408
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-09-11 15:59:16 +0900

    support tsv

--- index.js
+++ index.js
@@ -1,76 +1,73 @@
 "use strict";
 
-(function(){
-  try {
-    if(window.console) void 0;
-    window.csvParser = Parser;
-  } catch {
-    global.csvParser = Parser;
-  };
-}());
-
-function Parser(options) {
-  var that = {};
+(function(self) {
+  self = Object.create(null);
   
-  if(!options) options = {};
-  var safe = options.safe;
+  function Parser(type) {
+    var sep = "";
+    type = type.toLowerCase();
+    if(type === "tsv") {
+      sep = "\t"
+    } else if(type === "csv") {
+      sep = ","
+    };
   
-  Object.defineProperty(that, "parse", {
-    value: function parse(input, reviver) {
-      if(typeof reviver !== "function") reviver = new Function('return void 0;');
-        return input.split("\n")
-          .map(
-            function(x) {
-              return reviver(x, 0) || x.split(",")
-            }
-          )
-          .map(
-            function(x) {
-              return reviver(x, 1) || 
-              x.filter(
-                function(y) {
-                  return reviver(y, 2) ||
-                    y !== ""
-                }
-              )
-            }
-          )
-          .map(
-            function(x) {
-              return reviver(x, 3) || 
-                x.map(
+    Object.defineProperty(self, "parse", {
+      value: function parse(input, reviver) {
+        if(typeof reviver !== "function") reviver = new Function('return void 0;');
+          return input.split("\n")
+            .map(
+              function(x) {
+                return reviver(x, 0) || x.split(sep)
+              }
+            )
+            .map(
+              function(x) {
+                return reviver(x, 1) || 
+                x.filter(
                   function(y) {
-                    return reviver(y, 4) ||
-                      y.replace("\r", "")
+                    return reviver(y, 2) ||
+                      y !== ""
                   }
-              )
-            }
-          )
-          .filter(
-            function(x) {
-              return !!x && !!x.length
-            }
-        );
-    }
-  });
+                )
+              }
+            )
+            .map(
+              function(x) {
+                return reviver(x, 3) || 
+                  x.map(
+                    function(y) {
+                      return reviver(y, 4) ||
+                        y.replace("\r", "")
+                    }
+                )
+              }
+            )
+            .filter(
+              function(x) {
+                return !!x && !!x.length
+              }
+          );
+      }
+    });
   
-  Object.defineProperty(that, "stringify", {
-    value: function stringify(input){
-      return input.map(
-        function(x) {
-          return x.map(
-            function(y) {
-              if(safe) return y.replace(/,/g, "");
-              return y;
-            }
-          )
-          .join();
-        }
-      )
-        .join("\n");
-    }
-  });
+    Object.defineProperty(self, "stringify", {
+      value: function stringify(input){
+        return input.map(
+          function(x) {
+            return x.map(
+              function(y) {
+                if(safe) return y.replace(RegExp(sep, "g"), "");
+                return y;
+              }
+            )
+            .join();
+          }
+        )
+          .join("\n");
+      }
+    });
   
-  Object.freeze(that);
-  return that;
-};
+    Object.freeze(self);
+  };
+})((global && global.csvParser) || (window && window.csvParser));
